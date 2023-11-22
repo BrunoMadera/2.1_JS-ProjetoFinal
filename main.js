91c0a991a0beb53557290db0ed66b93d2de11d09
BrunoMadera <brunomadera@gmail.com>
2023-11-22 20:06:20 -0300

    icon

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
+// //icone do Help
+
+
+
+
+// function abrirHelp(){
+
+// import Swal from 'sweetalert2';
+
+//         Swal.fire({
+//         icon: "error",
+//         title: "Oops...",
+//         text: "Something went wrong!",
+//         footer: '<a href="#">Why do I have this issue?</a>'
+//         });
+// }
 
 // verificando Status do Usuario
 
